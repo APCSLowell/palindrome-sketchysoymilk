0eedd36bf1ed5afbdfd71e60f2926cf4c8ecb920
sketchysoymilk <143027784+sketchysoymilk@users.noreply.github.com>
2024-01-31 12:47:06 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,64 +6,6 @@ public class Main {
 		PalindromeChecker sim = new PalindromeChecker();
         	sim.tester();
 	}
-	public void setup()
-{
-  String lines[] = loadStrings("palindromes.txt");
-  System.out.println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++) 
-  {
-    if (palindrome(lines[i])==true)
-    {
-      System.out.println(lines[i] + " IS a palindrome.");
-    } else
-    {
-      System.out.println(lines[i] + " is NOT a palindrome.");
-    }
-  }
-}
-public String noSpaces(String sWord) {
-  String nS = "";
-  for (int i = 0; i < sWord.length(); i++) {
-    if (sWord.charAt(i) == ' ')
-      nS = nS + "";
-    else
-      nS = nS + sWord.charAt(i);
-  }
-  return nS;
-}
-public String onlyLetters(String sString) {
-  String oL = "";
-  for (int i = 0; i < sString.length(); i++) {
-    if (Character.isLetter(sString.charAt(i)))
-      oL = oL + sString.charAt(i);
-  }
-  return oL;
-}
-public String noCaps(String sWord){
-  return sWord.toLowerCase();
-}
-
-public boolean palindrome(String word) {
-  word = noSpaces(word);
-  word = onlyLetters(word);
-  word = noCaps(word);
-  String hi = "";
-  for (int i = word.length(); i > 0; i--) {
-    hi = hi + word.substring(i-1, i);
-  }
-  if (hi.equals(word))
-    return true;
-  return false;
-}
-public String reverse(String str)
-{
-  String sNew = new String();
-  for (int i = str.length(); i > 0; i--) {
-    sNew = sNew + str.substring(i-1, i);
-  }
-  return sNew;
-}
-
 
 }
 
